59529afbfa138cc379cc3651db06c70aef2195d7
DunNo268 <ngoquocdung2208@gmail.com>
2022-12-16 17:23:42 +0700

    Antique

---
+++ javascripts/subscription.js
@@ -0,0 +1,10 @@
+  function submit() {
+  let x = document.getElementById("name").value;
+  let text;
+  if (isNaN(x) || x < 1 || x > 10) {
+    text = "Input not valid";
+  } else {
+    text = "Input OK";
+  }
+  document.getElementById("output").innerHTML = text;
+}
